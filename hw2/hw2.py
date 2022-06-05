44517ac19a7f5ef6895ed360c23eaec9c659b4b7
Val7117 <valik2105@mail.ru>
2022-06-05 14:03:18 +0300

    hw2 is changed

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3                                                                 
 
 entered_data = input("-> ")
 splitted_data = entered_data.split()
-
-result = []
-
-for i in splitted_data:
-    if i in  splitted_data and  i not in result:
-        result.append(i)
+result = list(dict.fromkeys(splitted_data))
 
 print(' '.join(result))
+
