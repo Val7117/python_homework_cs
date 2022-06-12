55da4dac61168a1dfbed3c1acb3f1b2205ca0c9f
Val7117 <valik2105@mail.ru>
2022-06-12 15:15:14 +0300

    hw12 is editted

--- hw12/hw12.py
+++ hw12/hw12.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 
-# Fibonacci number generator function, where n is the number of the first n numbers of Fibonacci sequence.
-# First IF statement checks, if the entered number n in natural.
-# If n == 1 then print 1. If n == 2 then print 1 1. Otherwise, generate numbers and print the result.
+# Fibonacci number generator function, where n is the number of the n-th element of Fibonacci sequence.
+# First IF statement checks, if the entered number n is natural.
+# If n == 1 then print the first element of Fibonacci sequence.
+# If n == 2 then print the second element of Fibonacci sequence.
+# Otherwise, generate the n-th number and print the result.
 
 def fib(n):
     res = [1, 1]
-    string = '1 1'
     if n <= 0:
         print('Incorrect number. Please enter natural number.')
     elif n == 1:
-        print("The first number of Fibonacci sequence")
-        print('1')
+        print("The first number of Fibonacci sequence is 1")
     elif n == 2:
-        print("The first 2 numbers of Fibonacci sequence")
-        print(string)
+        print("The second number of Fibonacci sequence is 1")
     else:
         for i in list(range(1, n - 1)):
             res.append(res[i] + res[i-1])
-            string = string + ' ' + str(res[i] + res[i-1])
-        print(f"The first {n} numbers of Fibonacci sequence")
-        print(string)
 
-fib(20)
+        print(f"The {n}-th number of Fibonacci sequence is", res[-1])
+
+fib(5432)
