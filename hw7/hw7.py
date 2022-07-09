aa664d1c0250ad156d4dbeeafbc0be8be0bb660c
Val7117 <valik2105@mail.ru>
2022-07-09 16:36:33 +0300

    hw7.1 modified

--- hw7/hw7.py
+++ hw7/hw7.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-# Create empty list, string and dict
+# Define the range of UID to search for [uid_start, uid_end].
+uid_start = 1000
+uid_end = 10000
 
+# Create empty list, string and dict
 shells = []
 string = ''
 uids = dict()
@@ -23,13 +26,13 @@ with open("passwd", "r") as passwd_file:
 unique_shells = dict.fromkeys(shells)
 
 # For keys (shells) in dictionary count the number of occurrence for each shell
-# Create a string with the same name and add info from the old string + adding the name of a each shell
+# Create a string with the same name and add info from the old string + adding the name of each shell
 # and number of its occurrence
 
 for i in unique_shells:
     num = shells.count(i)
     string = string + i + ' - ' + str(num) + ' ; '
-
+string = string[0: -3]      # Get rid of the last space and semicolon '; '
 # Create an empty group_dict
 
 group_dict = dict()
@@ -41,35 +44,37 @@ group_dict = dict()
 with open("group", "r") as group_file:
     gr_lines = group_file.readlines()
     for gr_line in gr_lines:
-        if gr_line.split(":")[-1][:-1]:
-            user_name = gr_line.split(":")[-1][:-1]
-            user_name = user_name.split(',')
-            group_name = gr_line.split(":")[0]
-            group_dict.update({group_name: user_name})
+        user_name = gr_line.split(":")[-1][:-1]
+        user_name = user_name.split(',')
+        group_name = gr_line.split(":")[0]
+        group_dict.update({group_name: user_name})
 
-# Create and empty additional strings for the result and group
-
+# Create empty additional strings for the result and group
 res = ''
 gr = ''
+# Create empty dictionary for user's UIDs.
+users_uids = dict()
 
-# Go through group_dict.keys and group_dict values and create a new string adding the old 'res' string and
-# a new pair of group's name and users' UIDs.
-# There is also an additional check for the right output. If the previous group's name is the same as current,
-# then only add ',' and user's UID. Otherwise add group's name and user's UID.
+# Select the needed UIDs from 'uids' dictionary and always search for the 'root'.
+for uid in uids:
+    if uid == 'root' or (uid_start <= int(uids[uid]) <= uid_end):
+        users_uids.update({uid: uids[uid]})
 
-for group in group_dict.keys():
-    gr_new = group
-    for i in group_dict[group]:
-        if gr_new == gr:
-            res = res + ',' + f'{uids[i]}'
-        else:
-            res = res + f'{group}' + ':' + f'{uids[i]}'
-            gr = gr_new
-    res = res + " "
+# Collect needed groups and UIDs and create the resulting string.
+for group in group_dict:
+    for uid in users_uids:
+        if uid in group_dict[group] or uid == group:
+            if group == gr:
+                res = res + f"{users_uids[uid]}, "
+            else:
+                res = res + f"{group}:" + f"{users_uids[uid]}, "
+                gr = group
+res = res[0:-2]     # Get rid of the last comma in the string.
 
 # Write shells and the number of users, groups and users' UIDs to "output.txt" file.
-
 with open("output.txt", "w") as output:
     output.write(string)
     output.write('\n')
+    output.write('\n')
     output.write(res)
+    output.write('\n')
